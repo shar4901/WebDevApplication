458800d90500dc6e243b71ef65950f00990d2638
shar4901 <90164014+shar4901@users.noreply.github.com>
2022-01-28 23:40:39 -0700

    final

--- Assignment_4_Movies/Controllers/HomeController.cs
+++ Assignment_4_Movies/Controllers/HomeController.cs
@@ -12,9 +12,8 @@ namespace Assignment_4_Movies.Controllers
 {
     public class HomeController : Controller
     {
-        //private ILogger<HomeController> _logger;
 
-        private StudentFormContext _blahContext { get; set; }
+        private StudentFormContext _blahContext;
         //constructor
         public HomeController(ILogger<HomeController> logger, StudentFormContext someName)
         {
@@ -23,18 +22,133 @@ namespace Assignment_4_Movies.Controllers
 
 
 
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        //Index page controller
         public async Task<IActionResult> Index()
         {
             var student = await _blahContext.student.ToListAsync();
             return View(student);
         }
 
-        //edit controller
+        //Delete student index page
+        [HttpPost]
+        public async Task<IActionResult> Index(StudentModel students)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var exist = _blahContext.student.Where(x => x.student_id == students.student_id).FirstOrDefault();
 
+                    if (exist != null)
+                    {
+                        _blahContext.Remove(exist);
+                        await _blahContext.SaveChangesAsync();
+
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, $"Something went wrong {ex.Message}");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, $"Something went wrong, invalid model");
+
+            return View();
+        }
+
+
+        //Edit Student controller
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var exist = await _blahContext.student.Where(x => x.student_id == id).FirstOrDefaultAsync();
+
+            return View(exist);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(StudentModel studentInstance)
+        {
+            // validate that our model meets the requirement
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Check if the contact exist based on the id
+                    var exist = _blahContext.student.Where(x => x.student_id == studentInstance.student_id).FirstOrDefault();
+
+                    // if the contact is not null we update the information
+                    if (exist != null)
+                    {
+                        exist.first_name = studentInstance.first_name;
+                        exist.last_name = studentInstance.last_name;
+                        exist.street_address = studentInstance.street_address;
+                        exist.city = studentInstance.city;
+                        exist.state = studentInstance.state;
+                        exist.zip = studentInstance.zip;
+                        exist.phone_number = studentInstance.phone_number;
+                        exist.email = studentInstance.email;
+                        exist.category = studentInstance.category;
+
+                        // we save the changes into the db
+                        await _blahContext.SaveChangesAsync();
+
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, $"Something went wrong {ex.Message}");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, $"Something went wrong, invalid model");
+
+            return View(studentInstance);
+        }
+
+        //Delete Student controller
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var exist = await _blahContext.student.Where(x => x.student_id == id).FirstOrDefaultAsync();
+
+            return View(exist);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(StudentModel students)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var exist = _blahContext.student.Where(x => x.student_id == students.student_id).FirstOrDefault();
+
+                    if (exist != null)
+                    {
+                        _blahContext.Remove(exist);
+                        await _blahContext.SaveChangesAsync();
+
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, $"Something went wrong {ex.Message}");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, $"Something went wrong, invalid model");
+
+            return View();
+        }
+
+
+
+
+        //Create Student Controller
         [HttpGet]
         public IActionResult CreateStudent()
         {
